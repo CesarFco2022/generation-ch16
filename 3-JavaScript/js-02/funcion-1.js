9983a9c80a8f2de1fbd8b0d4bc642269b2654c88
CesarFco2022 <matarodriguezcesarfrancisco@gmail.com>
2022-08-02 17:33:58 -0500

    firt commid

---
+++ 3-JavaScript/js-02/funcion-1.js
@@ -0,0 +1,26 @@
+function misuma(a,b) {
+    let resultado= (a + b);
+    console.log("el resulado es = " + resultado);
+}
+
+misuma(15,10);
+
+function misuma(a,b) {
+    
+    console.log("el resulado es = " + (a + b));
+}
+
+misuma(3,4);
+function miNombre(x,y,z){
+    //console.log(x " " + " " y + " " z);
+}
+
+function division(a,b) {
+    return a/b;
+}
+let a = "Cesar";
+let b = "Mata";
+let c = "Rodriguez";
+miNombre(a,b,c)
+
+division(10,5)
